8283495b4a2e3ac0d39a2123545f72fcd8775691
gabe <GabeMaker@users.noreply.github.com>
2015-05-09 22:43:46 +0100

    refactor

--- lib/euler2.rb
+++ lib/euler2.rb
@@ -1,11 +1,8 @@
-def fibs_to(number_in_sequence)
-  first_three = [1,2,3]
-  if number_in_sequence <= 3
-    return first_three[0, number_in_sequence]
+def fibs_to(nth)
+  first_two = [1,2]
+  return first_two[0, nth] if nth <= 2
+  (nth - 2).times do |x|
+    first_two << first_two[-1] + first_two[-2]
   end
-
-  (number_in_sequence - 3).times do |x|
-    first_three << first_three[-1] + first_three[-2]
-  end
-  first_three
+  first_two
 end
