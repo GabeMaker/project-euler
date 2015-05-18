22a10434fda953accd2375aa7274c0c30dc6f54e
gabe <GabeMaker@users.noreply.github.com>
2015-05-18 22:38:13 +0100

    create #prime?

---
+++ lib/euler3.rb
@@ -0,0 +1,5 @@
+class Fixnum
+  def prime?
+    true
+  end
+end
---
+++ spec/euler3_spec.rb
@@ -0,0 +1,11 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+require 'euler3'
+
+describe 'Problem 3' do
+  it 'can identify when a number is prime' do
+    expect(2.prime?).to eq true
+  end
+end
